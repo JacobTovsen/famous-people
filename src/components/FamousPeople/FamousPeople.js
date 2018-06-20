6f1354e6634c0e57f5ad40c99e91cc3b907764ab
Jacob Tovsen <jacobtovsen@Jacobs-MacBook-Pro.local>
2018-06-20 10:50:11 -0500

    playing with arrays

--- src/components/FamousPeople/FamousPeople.js
+++ src/components/FamousPeople/FamousPeople.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
 class FamousPeople extends Component {
-  
+
+    famousPeople = [];
+    
     constructor(props) {
         super(props);
         this.state = { FamousPerson: { name: 'Famous Person', role: 'What Theyre Famous For' }}
     }
     
     handleChangeFor = (propertyName) => (event) => {
-        console.log(propertyName);
+        // console.log(propertyName);
         this.setState( { FamousPerson: {
             ...this.state.FamousPerson,
             [propertyName]: event.target.value
@@ -16,13 +18,21 @@ class FamousPeople extends Component {
         });
     }
   
+    logName = (event) => {
+        console.log(this.state.FamousPerson);
+        this.famousPeople.push(this.state.FamousPerson);
+        console.log(this.famousPeople);
+    }
+
     render() {
         return (
             <div>
-                <h2>Famous People</h2>
-                <input onChange={this.handleChangeFor('name')} />
-                <input onChange={this.handleChangeFor('role')} />
+                <input placeholder="Famous Person" onChange={this.handleChangeFor('name')} />
+                <p>  </p>
+                <input placeholder="Famous Role" onChange={this.handleChangeFor('role')} />
                 <p>{this.state.FamousPerson.name} is famous for the role of {this.state.FamousPerson.role}.</p>
+                <button onClick={this.logName}>Submit
+                </button>
             </div>
         );
     }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Famous People</h1>
         </header>
         
     );
